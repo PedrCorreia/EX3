d8a801c8f18a8d35976b666ecf39390cf24275f0
Anna-sf12 <annasf22@ru.is>
2025-11-01 14:54:12 +0100

    Make

---
+++ assignment/Q3/Q3.m
@@ -0,0 +1,8 @@
+%% Q3
+
+% Filter a delta function signal with a length of 64 samples with the stable filter from question 1. Make
+% a Fourier transform of the impulse response and compare this transfer function to the one calculated
+% with your function transfer. Make plots of the transfer function with an amplification in dB. Are
+% the two transfer functions exactly alike and if not, why?
+
+
--- assignment/Q4/Q4.m
+++ assignment/Q4/Q4.m
@@ -1,1 +1,9 @@
 %% Q4
+
+% There are many functions for designing both FIR filters and IIR filters. The desired amplification, 
+% cutoff frequencies and filter order is given to the routines, and the output is the filter coefficients. One of
+% these is the function firpm in Matlab (scipy.signal.remez in Python), that designs linear phase
+% FIR filters. Use this routine to design a low-pass FIR filter with a pass band up to 500 Hz and a stop
+% band after 570 Hz. Use a sampling frequency of 2000 Hz and design a filter with an order that you
+% determine to obtain a stop-band attenuation of 45 dB. Make a procedure that inputs all the parameters
+% and plots the resulting transfer function with amplification in dB. Which order is necessary?
