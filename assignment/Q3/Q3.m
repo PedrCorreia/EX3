70d200f0586eac157b6cb245c448316f8fd40b11
Anna-sf12 <annasf22@ru.is>
2025-11-01 15:03:38 +0100

    M problem

--- assignment/Q3/Q3.m
+++ assignment/Q3/Q3.m
@@ -1,8 +1,17 @@
 %% Q3
-
+clear, close, clc;
 % Filter a delta function signal with a length of 64 samples with the stable filter from question 1. Make
 % a Fourier transform of the impulse response and compare this transfer function to the one calculated
 % with your function transfer. Make plots of the transfer function with an amplification in dB. Are
 % the two transfer functions exactly alike and if not, why?
+
+%% Define the delta function
+delta = zeros(1,64); % Function of length 64 
+delta(1,1) = 1; % with one spike corresponding to dirac(x) 
+
+
+
+
+
 
 
